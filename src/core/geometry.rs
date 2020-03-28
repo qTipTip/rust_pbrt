6c4c9efc8a569cefa2b61d41bb2af45fab6dac00
Ivar Stangeby <istangeby@gmail.com>
2020-03-28 10:46:46 -0700

    Added preliminary ops::Add implementation for Vector2<T>

--- src/core/geometry.rs
+++ src/core/geometry.rs
@@ -1,11 +1,21 @@
-struct Vector2<T> {
+use std::ops;
+
+pub struct Vector2<T> {
     x: T,
     y: T,
 }
 
-struct Vector3<T> {
+pub struct Vector3<T> {
     x: T,
     y: T,
     z: T,
 }
 
+// the where clause can alternatively be written as impl<T: Copy + Add<T, Output=T>
+// I.e, this is implemented only for generic types implementing Copy and Add:T->T.
+impl<T> ops::Add for Vector2<T> where T: Copy + ops::Add<T, Output=T> {
+    type Output = Vector2<T>;
+    fn add(self, rhs: Vector2<T>) -> Vector2<T> {
+        Vector2 { x: self.x + rhs.x, y: self.y + rhs.y }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use core::geometry::Vector2;
 fn main() {
     println!("Hello, world!");
+
+    let pt = Vector2 { x: 2.0f32, y: 3.2f32 };
 }

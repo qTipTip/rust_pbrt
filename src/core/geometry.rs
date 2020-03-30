1dfa0d0235bb68462340508bb5843558b2c4ea39
Ivar Stangeby <istangeby@gmail.com>
2020-03-30 04:58:07 -0700

    Implemented all vector2-ops for vector3.

--- src/core/geometry.rs
+++ src/core/geometry.rs
@@ -121,7 +121,7 @@ impl<T> ops::Index<u32> for Vector2<T> {
     }
 }
 
-pub fn dot<T>(a: Vector2<T>, b: Vector2<T>) -> T where T: ops::Mul<Output=T> + ops::Add<Output=T> {
+pub fn vec2_dot<T>(a: Vector2<T>, b: Vector2<T>) -> T where T: ops::Mul<Output=T> + ops::Add<Output=T> {
     a.x * b.x + a.y * b.y
 }
 
@@ -131,7 +131,7 @@ mod vector2_tests {
 
     use num_traits::Float;
 
-    use crate::core::geometry::{dot, Vector2};
+    use crate::core::geometry::{vec2_dot, Vector2};
 
     #[test]
     fn test_constructor_nan_float() {
@@ -278,7 +278,7 @@ mod vector2_tests {
     #[test]
     fn test_dot() {
         let v1 = Vector2 { x: -1.0, y: 2.0 };
-        assert_eq!(dot(v1, v1), 5.0);
+        assert_eq!(vec2_dot(v1, v1), 5.0);
         assert_eq!(v1.dot(v1), 5.0);
     }
 }
@@ -289,3 +289,120 @@ pub struct Vector3<T> {
     pub y: T,
     pub z: T,
 }
+
+// This NaN-check is only implemented for Float-types.
+impl<T> Vector3<T> where T: Float {
+    pub fn new(x: T, y: T, z: T) -> Self {
+        assert!(
+            !x.is_nan() && !y.is_nan() && !z.is_nan()
+        );
+        Vector3 { x: x, y: y, z: z }
+    }
+}
+
+impl<T> Vector3<T> {
+    pub fn length_squared(self) -> T where T: ops::Mul<T, Output=T> + ops::Add<T, Output=T> + Copy {
+        self.x * self.x + self.y * self.y + self.z * self.z
+    }
+
+    pub fn length(self) -> T where T: num_traits::Float {
+        self.length_squared().sqrt()
+    }
+
+    pub fn has_nans(self) -> bool where T: num_traits::Float {
+        self.x.is_nan() || self.y.is_nan() || self.z.is_nan()
+    }
+
+    pub fn abs(self) -> Vector3<T> where T: num_traits::sign::Signed {
+        Vector3 { x: self.x.abs(), y: self.y.abs(), z: self.z.abs() }
+    }
+
+    pub fn dot(self, rhs: Vector3<T>) -> T where T: ops::Mul<Output=T> + ops::Add<Output=T> {
+        self.x * rhs.x + self.y * rhs.y + self.z * rhs.z
+    }
+}
+
+
+// the where clause can alternatively be written as impl<T: Copy + Add<T, Output=T>
+// I.e, this is implemented only for generic types implementing Copy and Add:T->T.
+impl<T> ops::Add for Vector3<T> where T: Copy + ops::Add<T, Output=T> {
+    type Output = Vector3<T>;
+    fn add(self, rhs: Vector3<T>) -> Vector3<T> {
+        Vector3 { x: self.x + rhs.x, y: self.y + rhs.y, z: self.z + rhs.z }
+    }
+}
+
+// Note that we require self to be mutable in order to alter its internal state.
+impl<T> ops::AddAssign for Vector3<T> where T: ops::AddAssign {
+    fn add_assign(&mut self, rhs: Vector3<T>) {
+        self.x += rhs.x;
+        self.y += rhs.y;
+        self.z += rhs.z;
+    }
+}
+
+impl<T> ops::Sub for Vector3<T> where T: Copy + ops::Sub<T, Output=T> {
+    type Output = Vector3<T>;
+    fn sub(self, rhs: Vector3<T>) -> Vector3<T> {
+        Vector3 { x: self.x - rhs.x, y: self.y - rhs.y, z: self.z - rhs.z }
+    }
+}
+
+// Note that we require self to be mutable in order to alter its internal state.
+impl<T> ops::SubAssign for Vector3<T> where T: ops::SubAssign {
+    fn sub_assign(&mut self, rhs: Vector3<T>) {
+        self.x -= rhs.x;
+        self.y -= rhs.y;
+        self.z -= rhs.z;
+    }
+}
+
+impl<T> ops::Mul<T> for Vector3<T> where T: Float {
+    type Output = Vector3<T>;
+
+    fn mul(self, rhs: T) -> Self::Output {
+        Vector3 { x: self.x * rhs, y: self.y * rhs, z: self.z * rhs }
+    }
+}
+
+impl<T> ops::MulAssign<T> for Vector3<T> where T: Copy + ops::MulAssign {
+    fn mul_assign(&mut self, constant: T) {
+        self.x *= constant;
+        self.y *= constant;
+        self.z *= constant;
+    }
+}
+
+impl<T> ops::Div<T> for Vector3<T> where T: Float {
+    type Output = Vector3<T>;
+
+    fn div(self, rhs: T) -> Vector3<T> {
+        self * rhs.recip()
+    }
+}
+
+impl<T> ops::Neg for Vector3<T> where T: Copy + ops::Neg<Output=T> {
+    type Output = Vector3<T>;
+
+    fn neg(self) -> Self::Output {
+        Vector3 { x: -self.x, y: -self.y, z: -self.z }
+    }
+}
+
+impl<T> ops::Index<u32> for Vector3<T> {
+    type Output = T;
+
+    fn index(&self, index: u32) -> &Self::Output {
+        match index {
+            0 => &self.x,
+            1 => &self.y,
+            2 => &self.z,
+            _ => panic!("Index out of bounds for Vector2"),
+        }
+    }
+}
+
+pub fn vec3_dot<T>(a: Vector3<T>, b: Vector3<T>) -> T where T: ops::Mul<Output=T> + ops::Add<Output=T> {
+    a.x * b.x + a.y * b.y + a.z + b.z
+}
+
